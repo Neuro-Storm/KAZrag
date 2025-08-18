e5c02fb6c5443153deabea28e49fca6b7663cb05
Neuro-Storm <faraonligin@yandex.ru>
2025-08-18 22:14:23 +0300

    Отключение режима перезагрузки для отладки запуска сервера

--- main.py
+++ main.py
@@ -94,8 +94,7 @@ if __name__ == "__main__":
             "main:app",
             host="127.0.0.1",
             port=8000,
-            reload=True,
-            reload_excludes=["*.log", "logs/*", "*.log.*", "*.log~"]
+            reload=False
         )
         logger.info("Сервер запущен")
     except Exception as e:
